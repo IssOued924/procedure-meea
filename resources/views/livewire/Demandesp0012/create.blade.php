7f1ad8af2d588247ae8ab2b76d80b1ed2f08afda
Ouedraogo Josue <jos.weder@gmail.com>
2023-10-30 16:28:37 +0000

    usager modal view correction 1

--- resources/views/livewire/Demandesp0012/create.blade.php
+++ resources/views/livewire/Demandesp0012/create.blade.php
@@ -11,7 +11,7 @@
     <div class="container-fluid" id="grad1">
         <div class="row justify-content-center mt-0">
             <div class="col-11 col-sm-9 col-md-7 col-lg-10 text-center p-0 mt-3 mb-2">
-                <div class="card px-0 pt-4 pb-0 mt-3 mb-3">
+                <div class="cardd px-0 pt-4 pb-0 mt-3 mb-3">
                     <h5><strong>Demande de permis d'écotourisme ou tourisme écologique</strong></h5>
                    <p> @if(session('success'))
                     <div class="alert alert-success">
