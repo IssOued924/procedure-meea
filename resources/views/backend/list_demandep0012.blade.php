4406c2220b805a13abdcc99ae96a0cb12bd4aa7e
faycallenglengue200 <faycallenglengue200@gmail.com>
2023-12-07 18:15:03 +0100

    3

--- resources/views/backend/list_demandep0012.blade.php
+++ resources/views/backend/list_demandep0012.blade.php
@@ -97,12 +97,11 @@
                                         <th scope="col">#</th>
                                         <th scope="col">Date Demande</th>
                                         <th scope="col">Demandeur</th>
+
                                         <th scope="col">Résidence</th>
                                         <th scope="col">Etat Demande</th>
-                                        <th scope="col">Paiement</th>
                                         <th scope="col">Délai</th>
-                                        <th scope="col">Déposé</th>
-                                        <th scope="col">Assigné a</th>
+                                        <th scope="col">Paiement</th>
 
                                         <th scope="col">Action</th>
                                     </tr>
@@ -164,6 +163,15 @@
                                         <td>{{ $demande->localite->libelle }}</td>
 
                                         <td><span class="badge {{ $statutColor }} ">{{ $statut}}</span> </td>
+                                        @if (isset($demande->delai))
+
+
+                                        <td><span class="badge bg-dark">{{
+
+                                        $demande->delai}} </span> Jours </td>
+                                        @else
+                                        <td><span class="  ">-</span> </td>
+                                        @endif
 
                                         {{-- partie paiement --}}
                                         @if ($demande->paiement === 1)
@@ -173,15 +181,7 @@
                                         <td><b><span class="text-warning">Non Payée</span></b></td>
                                         @endif
 
-                                        <td><span class="badge bg-dark">{{ $demande->procedure->delai}} </span> Jours </td>
 
-                                        <td>{{ $demande->created_at->diffForHumans() }}</td>
-
-                                        @if($demande->last_agent_assign != null)
-                                        <td> <span class="badge bg-primary"> {{ $demande->agent->nom. " " .$demande->agent->prenom}} </span> </td>
-                                        @else
-                                        <td> <span class="badge bg-danger"> non assigné </span> </td>
-                                        @endif
 
                                         <td>
                                             <button title="Voir Détail" type="button" class="btn btn-primary "
@@ -315,26 +315,23 @@
                                                             </button>
                                                         </div>
                                                         <div class="modal-body">
-                                                            <form method="post" enctype="multipart/form-data" action="{{ route('assignation', ['model' =>'AffectationP0012', 'idDemande' => $demande->uuid ,'nameDemandeId'=> 'demande_p0012_id', 'tableName'=>'demande_p0012_s'] ) }}">
+                                                            <form method="put"
+                                                                action="{{ route('statusChange', ['id' =>$demande->uuid, 'currentStatus' => $demande->etat ,'table'=> 'demande_p0012_s'] ) }}">
                                                                 @csrf
-
+                                                                @method('GET')
 
 
                                                                 <div class="form-group">
                                                                     <div class="text-center">
-                                                                        <h5>Choisir le collaborateur à assigné</h5>
+                                                                      <h5>Choisir le collaborateur à assigné</h5>
 
-                                                                        <select name="agent_id" id="" class="form-select border-success">
+                                                                            <select name="" id="" class="form-select border-success">
+                                                                                @foreach ($agents as $agent)
 
-                                                                            @foreach ($agents as $agent)
+                                                                                <option value="{{ $agent->uuid }}">{{ $agent->nom.' '.$agent->prenom }}</option>
+                                                                                @endforeach
 
-                                                                            @if($agent->service->libelle_court == $demande->procedure->service->libelle_court)
-                                                                            <option value="{{ $agent->uuid }}">{{ $agent->nom.' '.$agent->prenom }}</option>
-                                                                            @endif
-                                                                            
-                                                                            @endforeach
-
-                                                                        </select>
+                                                                            </select>
 
                                                                     </div>
 
