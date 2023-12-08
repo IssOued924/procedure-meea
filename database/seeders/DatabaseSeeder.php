82da34181a177836deb3bcc28c511bb795c7adba
faycallenglengue200 <faycallenglengue200@gmail.com>
2023-12-08 10:21:10 +0100

    2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,9 +19,9 @@ class DatabaseSeeder extends Seeder
             PaysSeeder::class,
             StatutDemandeSeeder::class,
             TypeUsagerSeeder::class,
-            RoleSeeder::class
+            RoleSeeder::class,
             //StructureSeeder::class
-           // UsersTableSeeder::class
+           UsersTableSeeder::class
         ]);
         // \App\Models\User::factory(10)->create();
 
