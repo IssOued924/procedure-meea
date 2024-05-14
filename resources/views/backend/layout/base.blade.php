c80ae330d427eac64bb032384cac8fa9303a980c
Ouedraogo Josue <jos.weder@gmail.com>
2024-05-14 12:29:20 +0000

    comment line

--- resources/views/backend/layout/base.blade.php
+++ resources/views/backend/layout/base.blade.php
@@ -44,7 +44,7 @@
 
     <!-- Bootstrap4 Duallistbox -->
     <!-- MDB -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.2/mdb.min.js"></script>
+    <!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.2/mdb.min.js"></script> -->
 
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
